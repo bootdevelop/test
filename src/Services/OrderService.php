4ce0e1cd6f2f85721e67bc956ba4b510b315d4aa
Luis <luis@bootdevelop.com>
2018-08-29 19:27:28 +0200

    new upload

--- src/Services/OrderService.php
+++ src/Services/OrderService.php
@@ -27,44 +27,44 @@ class OrderService
     }
 
 
-    private function createOrder() {
-        //https://developers.plentymarkets.com/api-doc/Order
-        /**
-         * @var $order Order
-         */
-        $order = pluginApp(Order::class);
-        $order->typeId = 1;
-        $order->lockStatus = 'unlocked';
-
-        $this->db->save($order);
-        return $order;
-    }
-
-
-    private function createAddress() {
-        //https://developers.plentymarkets.com/api-doc/account#account_models_address
-        /**
-         * @var $address Address
-         */
-        $address = pluginApp(Address::class);
-        $address->gender = 'male|female';
-        $address->name1 = "Company";
-        $address->name2 = "Luis Ferrer";
-        $address->name3 = "Last Name";
-        $address->name4 = "...";
-        //$address->companyName = "Bootdevelop";
-        //$address->firstName = "Luis";
-        //$address->lastName = "Ferrer";
-        $address->address1 = "...";
-        $address->address2 = "...";
-        $address->address3 = "...";
-        $address->address4 = "...";
-        $address->postalCode = "080172";
-        $address->countryId = 1;
-        $address->stateId = 1;
-        //$address->
-
-    }
+//    private function createOrder() {
+//        //https://developers.plentymarkets.com/api-doc/Order
+//        /**
+//         * @var $order Order
+//         */
+//        $order = pluginApp(Order::class);
+//        $order->typeId = 1;
+//        $order->lockStatus = 'unlocked';
+//
+//        $this->db->save($order);
+//        return $order;
+//    }
+//
+//
+//    private function createAddress() {
+//        //https://developers.plentymarkets.com/api-doc/account#account_models_address
+//        /**
+//         * @var $address Address
+//         */
+//        $address = pluginApp(Address::class);
+//        $address->gender = 'male|female';
+//        $address->name1 = "Company";
+//        $address->name2 = "Luis Ferrer";
+//        $address->name3 = "Last Name";
+//        $address->name4 = "...";
+//        //$address->companyName = "Bootdevelop";
+//        //$address->firstName = "Luis";
+//        //$address->lastName = "Ferrer";
+//        $address->address1 = "...";
+//        $address->address2 = "...";
+//        $address->address3 = "...";
+//        $address->address4 = "...";
+//        $address->postalCode = "080172";
+//        $address->countryId = 1;
+//        $address->stateId = 1;
+//        //$address->
+//
+//    }
 
 
 }
